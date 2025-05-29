3f7db271808a7c98f6deff6cb1b842d8949bfd84
thisisshiveshmishra <shiveshmishra74@gmail.com>
2025-05-29 14:14:58 +0530

    Added homepage and updated configurations and assets

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css'] // Only local styles here
+})
+export class HomepageComponent {}
